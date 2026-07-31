824144660d6857db8fd0ead373b18f59351d752a
ebstutoring2114 <ebstutoring2114@gmail.com>
2026-07-31 07:38:43 +0000

    deploy: b6b8fdd

---
+++ config.js
@@ -0,0 +1,4 @@
+window.EBSQ_CONFIG = {
+  url: "https://erniutzqogfcmzmxcsma.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVybml1dHpxb2dmY216bXhjc21hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUyMDc2NTUsImV4cCI6MjEwMDc4MzY1NX0.jm7PiQdHUxHWwiIWOERYz8BLg8l1rYwgAA1_ss2rVLs"
+};
